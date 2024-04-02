6ebe2cc1ee79b2873d55dca1cbfc3504edd49f7a
RaulM <raul.milchis@gmail.com>
2024-04-02 13:35:02 +0300

    DEV: Added assertions for if

--- interfaces/apb_interface.sv
+++ interfaces/apb_interface.sv
@@ -40,5 +40,68 @@ input   pclk,
 output  pready,
         prdata,
 );
+`ifdef ENABLE_ASSERTIONS
+
+  property pr_generic_not_unknown (signal) 
+    @(posedge pclk) disable iff(~preset_n)
+      !$isunknown(signal) ;
+  endproperty ;   
+
+  psel_never_X    : assert property (pr_generic_not_unknown(psel   )) else $display("[%0t] Error! psel is unknown (=X/Z)", $time);
+  pwrite_never_X  : assert property (pr_generic_not_unknown(pwrite )) else $display("[%0t] Error! pwrite is unknown (=X/Z)", $time);
+  penable_never_X : assert property (pr_generic_not_unknown(penable)) else $display("[%0t] Error! penable is unknown (=X/Z)", $time);
+  pready_never_X  : assert property (pr_generic_not_unknown(pready )) else $display("[%0t] Error! pready is unknown (=X/Z)", $time);
+  paddr_never_X   : assert property (pr_generic_not_unknown(paddr  )) else $display("[%0t] Error! paddr is unknown (=X/Z)", $time);
+  pwdata_never_X  : assert property (pr_generic_not_unknown(pwdata )) else $display("[%0t] Error! pwdata is unknown (=X/Z)", $time);
+  prdata_never_X  : assert property (pr_generic_not_unknown(prdata )) else $display("[%0t] Error! prdata is unknown (=X/Z)", $time);
+  // pstrb_never_X   : assert property (pr_generic_not_unknown(pstrb  )) else $display("[%0t] Error! pstrb is unknown (=X/Z)", $time);
+  // pprot_never_X   : assert property (pr_generic_not_unknown(pprot  )) else $display("[%0t] Error! pprot is unknown (=X/Z)", $time);
+  
+//paddr changed? -> transfer finished OR didn't start yet ((!psel))
+  property idle_state ;
+     @(posedge pclk) disable iff(!preset_n)
+        (!psel) |=> (!psel) or (psel && !penable) ;
+  endproperty
+
+  property setup_state ;
+     @(posedge pclk) disable iff(!preset_n)
+        (psel && !penable) |=> (psel && penable && !pready) or (psel && penable && pready) ;
+  endproperty
+
+  property access_wait_state ;
+    @(posedge pclk) disable iff(!preset_n)
+       (psel && penable && !pready) |=> (psel && penable && !pready) or (psel && penable && pready) ;
+  endproperty
+
+  property access_last_state ;
+    @(posedge pclk) disable iff(!preset_n)
+       (psel && penable && pready) |=> (!psel) or (psel && !penable) ;
+  endproperty
+
+  property pr_generic_stable(signal) 
+    @(posedge pclk) disable iff(!preset_n)
+       !$stable(signal) |-> (psel && !penable) or (!psel) ;
+  endproperty
+//Some signals need to be treated separately. pwdata needs be stable only during write transfers (pwrite=1):
+  property pwdata_in_wr_transfer ;  
+    @(posedge pclk) disable iff(!preset_n)
+       !$stable(pwdata) |-> (!pwrite) or ((psel && !penable) or (!psel)) ;
+  endproperty
+//The psel signal must be stable (=1) throughout the transfer, while penable signal must be stable (=1) during the whole access_phase.
+  property penable_in_transfer ;
+    @(posedge pclk) disable iff(!preset_n)
+    $fell(penable) |-> (!psel) or ($past(penable) && $past(pready)) ;
+  endproperty
+
+  property psel_stable_in_transfer ;
+    @(posedge pclk) disable iff(!preset_n)
+       !psel && $past(psel) |-> $past(penable) && $past(pready) ; 
+  endproperty    
+//Another signal that must be treated separately is PSTRB which must be low during read transfers
+  // property pstrb_low_at_read ;
+  //   @(posedge pclk) disable iff(!preset_n)
+  //      psel && !pwrite |-> pstrb == {(16/8){1'b0}} ;
+  // endproperty  
+`endif
 
 endinterface
--- interfaces/memory_interface.sv
+++ interfaces/memory_interface.sv
@@ -32,4 +32,49 @@ output  ack,
 inout   data
 );
 
+`ifdef ENABLE_ASSERTIONS
+//Checkers for req-ack protocol
+
+property ack_pulse; // Checks that ack is a pulse
+    @(posedge clk) disable iff (~rst_n)
+    $rose(ack) |=> ##1 $fell(ack);
+  endproperty
+  
+  property req_without_ack; // Request had no response from server side
+    @(posedge clk) disable iff (~rst_n)
+    (req & ~ack) |=> ~req;
+  endproperty
+  
+  property ack_without_req; // Ack without an active request 
+    @(posedge clk) disable iff (~rst_n)
+    (ack & ~req) |=> ~ack;
+  endproperty
+  
+  property ack_after_one_req_cc; //Acknowledge comes after one request HIGH clock cycle time
+    @(posedge clk) disable iff (~rst_n)
+    req & $changed(req) |-> ##1 ack;
+  endproperty
+  //Checkers for design specific
+  
+  // Assertion 1: The request activates correctly after a specific number of clock cycles
+  property req_dist_diff_than_constrained;
+    @(posedge clk) disable iff (~rst_n)
+    $fell(req) |-> ##REQ_DIST req;
+  endproperty
+  
+  always @(posedge clk) 
+    //REQ-ACK PROTOCOL CHECKERS
+    assert property (ack_pulse) else
+      $error("PROTOCOL VIOLATION: ack_pulse assertion failed! Acknowledgement signal isn't a pulse");
+    assert property (req_without_ack) else
+      $error("PROTOCOL VIOLATION: Next request came without an acknowledgement from server!");
+    assert property (ack_without_req) else
+      $error("PROTOCOL VIOLATION: Acknowledgement came without a request from client!");
+    assert property (ack_after_one_req_cc) else
+      $error("PROTOCOL VIOLATION: Acknowledge came the same time as request was asserted!");
+    //DESIGN FEATURES CHECKERS
+    assert property (req_dist_diff_than_constrained) else
+      $error("DESIGN SPECIFICATION MISMATCH: REQ_DIST is different than constrained distance!");
+  `endif
+
 endinterface
